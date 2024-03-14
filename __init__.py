d4943e0355e42f538393e4787d269fe88fba74c3
kazkboz <39435375+kazkboz@users.noreply.github.com>
2024-03-14 14:27:40 +0800

    Delete __init__.py

--- __init__.py
+++
@@ -1,4 +0,0 @@
-# --------------------------------------------------------
-# Copyright (C) 2020 NVIDIA Corporation. All rights reserved.
-# Nvidia Source Code License-NC
-# --------------------------------------------------------
